2da2e7d72e2509d1ead8b524d194ae3f4c81cfe8
Celina Vangstrup <au682953@uni.au.dk>
2022-03-10 12:49:05 +0100

    Corrected namespace

--- ClassLibraryUSBCharger/rfID/IReader.cs
+++ ClassLibraryUSBCharger/rfID/IReader.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleChargingBox.rfID
+﻿namespace ClassLibraryChargingBox.rfID
 {
     public interface IReader
     {
