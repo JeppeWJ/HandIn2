376cae101d0e02cdef65f8298abe44b2a136f137
JeppeWJ <202007790@post.au.dk>
2022-03-10 12:51:34 +0100

    Door classes added

---
+++ ClassLibraryUSBCharger/DoorClasses/DoorStateChangedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryChargingBox.DoorClasses
+{
+   public class DoorStateChangedEventArgs : EventArgs
+   {
+      public bool DoorStateChanged { get; set; }
+   }
+}
