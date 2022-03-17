d7e2407ec7924d1db7f6c294b644d25c857ab5ea
Mads Papendick Jensen <au665189@uni.au.dk>
2022-03-17 13:48:15 +0100

    Log added

---
+++ ClassLibraryUSBCharger/Logging/ILogging.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryChargingBox.Logging
+{
+    public interface ILogging
+    {
+        //void LogDoorLocked(string);
+        //void LogDoorUnlocked(string);
+
+
+    }
+}
