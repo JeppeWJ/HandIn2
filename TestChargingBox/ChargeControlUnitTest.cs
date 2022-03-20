0afe7f7024b7b66e3c19c46e9144d3e3bf2e0265
JeppeWJ <202007790@post.au.dk>
2022-03-20 14:05:53 +0100

    Unittest of ChargeControl.

---
+++ TestChargingBox/ChargeControlUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryChargingBox.ChargerClasses;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace TestChargingBox
+{
+   public class ChargeControlUnitTest
+   {
+      private ChargeControl _uut;
+      private IUsbCharger _currentSource;
+      [SetUp]
+      public void Setup()
+      {
+         _currentSource = Substitute.For<IUsbCharger>();
+         _uut = new ChargeControl(_currentSource);
+      }
+
+      [TestCase(0)]
+      [TestCase(100)]
+      [TestCase(200)]
+      public void CurrentChanged_DifferentCurrents_CurrentIsCorrect(double newCurrent)
+      {
+         _currentSource.CurrentValueEvent += Raise.EventWith(new CurrentEventArgs {Current = newCurrent});
+
+         Assert.That(_uut.CurrentValue, Is.EqualTo(newCurrent));
+      }
+   }
+}
