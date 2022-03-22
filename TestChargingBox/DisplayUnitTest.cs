803b3da20070d90c5bb2b9827ba861406d046ca6
Anders Skadhede Eriksen <au680670@uni.au.dk>
2022-03-22 21:38:59 +0100

    Displaytest

--- TestChargingBox/DisplayUnitTest.cs
+++ TestChargingBox/DisplayUnitTest.cs
@@ -1,38 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using ClassLibraryChargingBox.Display;
 
 namespace TestChargingBox
 {
-   public class DisplayUnitTest
-   {
-       private IDisplay _uut;
+    public class DisplayUnitTest
+    {
+        private IDisplay _uut;
+
         [SetUp]
         public void Setup()
         {
             _uut = new Display();
         }
 
-        [TestCase("Indlæs RFID", "Indlæs RFID")]
-        [TestCase("Tilslut oplader", "Tilslut oplader")]
-        [TestCase("Tilslutningsfejl", "Tilslutningsfejl")]
-        public void WriteToDisplayTest(string exp, string text)
+        [TestCase("Indlæs RFID")]
+        [TestCase("Tilslut oplader")]
+        [TestCase("Tilslutningsfejl")]
+        public void WriteToDisplayTest(string text)
         {
-            //string _textTrue = _uut.WriteToDisplay(text);
-            //Assert.That(exp, Is.EqualTo(_textTrue));
+            var stringwriter = new StringWriter();
+            Console.SetOut(stringwriter);
+            _uut.WriteToDisplay(text);
+            Assert.AreEqual(text + "\r\n", stringwriter.ToString());
+            Assert.Pass();
         }
 
-        [TestCase("Indlæs RFID", "Indlæs RFI")]
-        [TestCase("Tilslut oplader", "Tilslut oplades")]
-        [TestCase("Tilslutningsfelj", "Tilslutningsfejl")]
-        public void WriteToDisplayTes(string exp, string text)
-        {
-            //string _textFalse = _uut.WriteToDisplay(text);
-            //Assert.That(exp, Is.Not.EqualTo(_textFalse));
-        }
+
     }
 }
